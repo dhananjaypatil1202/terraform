0b38471ea2a1fe061ee60f9437e9174855965723
Dhananjay Patil <152286270+dhananjaypatil1202@users.noreply.github.com>
2024-03-19 16:58:24 +0530

    Update main.tf

--- motogp/modules/ec2_instance/main.tf
+++ motogp/modules/ec2_instance/main.tf
@@ -3,6 +3,7 @@ resource "aws_instance" "demo1" {
   ami = var.ami
   instance_type = var.instance_type
   subnet_id = var.subnet_id 
+  security_groups = var.security_groups
   key_name = var.key_name
   tags = {
     Name = "${var.project}-instance-${count.index}"
